7a36ac5a395ddf9b7b4dba5694de7162ddb3de4c
mitchgm <mitchgm@bgsu.edu>
2022-06-19 10:55:03 -0700

    added more dynamic html in the javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,41 @@
 var formEl = document.querySelector("#task-form");
+// this variable listens for the submit button to be clicked at the bottom, then targets whatever in the html doc has the id of task-form
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+// this variable edits whatever in the html has an id of tasks-to-do
 
 
 var createTaskHandler = function() {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // this variable targets an input button, with a name of task-name and looks only for the value of the input
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // this variable gets the calue of the selevct dropdown's picked option element in the html. Then updates the selector element with a name of task type with the value of the variable
+    
+
+    // this dynamically ads a li element to the html that has an id of task-item, its content is equal to the taskNameInput variable, and appends to the tasksToDoEl variable above which in turn goes to the task-to-do html id
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // creates list item ^^
+
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div. this dynamically creates an h3 for the task name and a span element for the task type within each div each time the function is called
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
   };
 
 
  formEl.addEventListener("submit", createTaskHandler);
-
+// this listens for a submit button to be click, then runs the createTaskhandler function
 
 
 
